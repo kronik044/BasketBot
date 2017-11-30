33ccfcc1b2f6ba9f25b0c3bfc3fdc3d2a44affa8
Al Lanovoy <kronik044@gmail.com>
2017-11-30 16:32:43 +0100

    s

--- basketBot/app/controller/api.js
+++ basketBot/app/controller/api.js
@@ -117,6 +117,7 @@ function nextSession(id, callback) {
       ret.setDate(ret.getDate() + (4 - 1 - ret.getDay() + 7) % 7 + 1);
     }
     callback(id,ret);
+    console.log("nextSession prc " + id + "date " ret);
     //return ret;
 }
 
