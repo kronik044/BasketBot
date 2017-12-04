cfaf67bdf6a35f23d0c0c7b9810ddeb879efe1c9
Al Lanovoy <kronik044@gmail.com>
2017-12-04 12:42:17 +0100

    ss

--- basketBot/app/controller/api.js
+++ basketBot/app/controller/api.js
@@ -19,8 +19,10 @@ exports.handleMessage = function(req, res) {
 	for (i = 0; i < messaging_events.length; i++) {
 		event = req.body.entry[0].messaging[i];
 		sender = event.sender.id;
-    //uPayload = event.message.quick_reply.payload;
-    //console.log("received payload " + uPayload);
+    if (event.message.quick_reply) {
+      uPayload = event.message.quick_reply.payload;
+      console.log("received payload " + uPayload);
+    }
     console.log("Message Received from client ");
     User.findOne({fb_id: sender}, function(err, user) {
       if (err) {
